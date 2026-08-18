c0f2af398ea5646588caa8395a2abb53cb0bfc48
Bs.dev <samuelbonfimaraujo9210@gmail.com>
2026-08-18 06:53:20 -0300

    feat: Update API_URL to dynamically set based on hostname

--- src/Api/Api_Address.js
+++ src/Api/Api_Address.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://192.168.15.52:3000";
+export const API_URL =
+    window.location.hostname.startsWith("192.168.")
+        ? "http://api.home.arpa:3000"
+        : "http://191.11.205.228:3000";
